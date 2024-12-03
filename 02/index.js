433b46bbada84e64d41d1708328339ce72f5de38
Johan Chrillesen <johan@chouic.com>
2024-12-03 13:23:20 +0100

    feat: add day 02

---
+++ 02/index.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+
+
+const getDataParsed = () => {
+    const data = fs.readFileSync('./input.txt', 'utf8');
+    const reports = data.split('\n');
+
+    return reports;
+}
+
+const performCheck = (levels) => {
+    let increase = null
+    for (let i = 0; i < levels.length - 1; i++) {
+        if (levels[i] < levels[i + 1]) {
+            if (increase === false) {
+                return false
+            }
+            increase = true
+        } else {
+            if (increase === true) {
+                return false
+            }
+            increase = false
+        }
+
+        const diff = Math.abs(levels[i] - levels[i + 1])
+
+        if (diff < 1 || diff > 3) {
+            return false
+        }
+    }
+    return true
+}
+
+const main = () => {
+    let reports = getDataParsed();
+
+    // First part
+    const nbSafe = reports.map((report) => {
+        let baselevels = report.split(' ').map((level) => parseInt(level))
+        let safe = null
+        let removeIndex = -1
+        while (safe !== true && removeIndex < baselevels.length) {
+            let levels = [...baselevels]
+            if (removeIndex !== -1) {
+                levels = levels.filter((_, index) => index !== removeIndex);
+            }
+            console.log(levels)
+            safe = performCheck(levels)
+            removeIndex++
+        }
+        return safe
+    }).filter((safe) => safe === true).length
+
+    console.log(nbSafe)
+}
+
+main();
